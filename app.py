d68b3a9e4fc1da3bd06bafa13169cb2735b084af
Ana Wagner <93949347+awanagner@users.noreply.github.com>
2025-11-25 08:03:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-
+##
 API_KEY = "CHAVE_API_AQUI"
 
 API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent"
@@ -108,6 +108,7 @@ def handle_verification():
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
 
 
 
